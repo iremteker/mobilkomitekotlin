bbe3ecb54fd871166d739d4796873e3ac0930e73
İrem Teker <147082141+iremteker@users.noreply.github.com>
2024-11-08 18:02:37 +0300

    a

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+
     id ("androidx.navigation.safeargs.kotlin") version "2.7.7" apply false
 
 }
